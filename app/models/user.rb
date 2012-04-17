71044c6bd5e6d72c01a422639c3ccde8754c3e29
Erik Azar <erik.azar@gmail.com>
2012-04-16 20:04:35 -0400

    I2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,36 +8,34 @@ class User < ActiveRecord::Base
 
   validate :password_must_be_present
 
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + "wibble" + salt)
+  end
+
+  def generate_salt
+    self.salt = self.object_id.to_s + rand.to_s
+  end
+
+  def password=(password)
+    @password = password
+
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+    end
+  end
+
+  def User.authenticate(name, password)
+    if user = find_by_name(name)
+      if user.hashed_password == self.encrypt_password(password, user.salt)
+        user
+      end
+    end
+  end
+
   private
 
     def password_must_be_present
       errors.add(:password, "Missing password") unless hashed_password.present?
-    end
-
-  public
-
-    def User.encrypt_password(password, salt)
-      Digest::SHA2.hexdigest(password + "wibble" + salt)
-    end
-
-    def generate_salt
-      self.salt = self.object_id.to_s + rand.to_s
-    end
-
-    def password=(password)
-      @password = password
-
-      if password.present?
-        generate_salt
-        self.hashed_password = self.class.encrypt_password(password, salt)
-      end
-    end
-
-    def User.authenticate(name, password)
-      if user = find_by_name(name)
-        if user.hashed_password == self.class.encrypt_password(password, user.salt)
-          user
-        end
-      end
     end
 end
